40eb616c05208e017881ac4f999944e81ce5a089
vincesanityyy <vbustillo97@gmail.com>
2020-04-04 21:00:53 +0800

    terms and conditions

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -163,6 +163,18 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <div class="col-md-4 col-form-label text-md-right"></div>
+                            <div class="col-md-6">
+                              <div class="form-check">
+                                <input required class="form-check-input" type="checkbox" id="gridCheck1">
+                                <label class="form-check-label" for="gridCheck1">
+                                  Terms and Conditions
+                                </label>
+                              </div>
+                            </div>
+                          </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button style="background-color: #8B0000; color: white" type="submit" class="btn">
